2411382d28c73c15760bd2d911214fd2707d596b
AhmadOmar <ahmad.omar@zcoderz.com>
2023-09-11 10:45:06 +0300

    fix bug " 'max_length' is         # too small to fit the longest value in 'choices' (8 characters)."

--- languages/fields.py
+++ languages/fields.py
@@ -1,14 +1,20 @@
 from django.db.models.fields import CharField
 
+
 class LanguageField(CharField):
     """
     A language field for Django models.
     """
+
     def __init__(self, *args, **kwargs):
         # Local import so the languages aren't loaded unless they are needed.
         from .languages import LANGUAGES
+        # fix max length
+        # actual error bug
+        # user_settings.CoreUserSettings.app_languages: (fields.E009) 'max_length' is
+        # too small to fit the longest value in 'choices' (8 characters).
 
-        kwargs.setdefault('max_length', 3)
+        kwargs.setdefault('max_length', 10)
         kwargs.setdefault('choices', LANGUAGES)
         kwargs.setdefault('db_collation', None)
 
